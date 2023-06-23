98d456b24c184d11beef50ad82d335668bf49494
Nadine de Lemos Aquino <nadinelemos2@gmail.com>
2023-06-22 21:23:56 -0300

    Quarto exercicio da leetCode

--- ValidParentheses.java
+++ ValidParentheses.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class ValidParentheses {
 	public static void main(String[] args) {
-		System.out.println(isValid("]")); // "[([]])"
+		System.out.println(isValid("]"));
 	}
 
 	public static boolean isValid(String s) {
