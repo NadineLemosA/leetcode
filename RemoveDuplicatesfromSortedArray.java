eb675d31487fe74b3225e7fcc899b1b1144c49f7
Nadine de Lemos Aquino <nadinelemos2@gmail.com>
2023-09-23 01:49:56 -0300

    wip - comparando arrays simples

--- RemoveDuplicatesfromSortedArray.java
+++ RemoveDuplicatesfromSortedArray.java
@@ -1,34 +1,39 @@
 package leetCode;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class RemoveDuplicatesfromSortedArray {
 	public static void main(String[] args) {
-		System.out.println(removeDuplicates(new int[] { 1, 1, 2, 3, 5, 5 }));
+		System.out.println(removeDuplicates(new int[] { 1, 1, 2, 3, 5, 5 })); // 1,1,1,2,2,2
 	}
 
 	public static int removeDuplicates(int[] nums) {
 		int k;
-		ArrayList<Integer> listaDeUnicos = new ArrayList<>();
+		int[] listaDeUnicos = new int[nums.length]; //  a lista deve ser arraylist e achar modo de comparar int com interger
 
-		listaDeUnicos.add(nums[0]);
+		listaDeUnicos[0] = nums[0];
 
-		for (int i = 0; i < nums.length; i++) {
-			System.out.println("i: " + listaDeUnicos.get(i));
-
-			for (int j = i + 1; j < nums.length; j++) {
-				System.out.println("j: " + nums[j]);
-
-				if (listaDeUnicos.get(i) != nums[j]) { // acho que tem coisa errada aqui
-					listaDeUnicos.add(nums[j]);
-				}
+		for (int i = 0; i < nums.length - 1; i++) {
+			// System.out.println("i: " + listaDeUnicos.get(i));
+			if (listaDeUnicos[i] != nums[i + 1]) {
+				listaDeUnicos[i + 1] = nums[i + 1];
 			}
-			System.out.println(listaDeUnicos);
+
+			// for (int j = i + 1; j < nums.length; j++) {
+			// System.out.println("j: " + nums[j]);
+
+			// if (listaDeUnicos.get(i) != nums[j]) {
+			// listaDeUnicos.add(nums[j]);
+			// }
+			// }
+
+			System.out.println(Arrays.toString(listaDeUnicos));
 			System.out.println();
 		}
+
 		System.out.println(listaDeUnicos);
 
-		k = listaDeUnicos.size();
+		k = listaDeUnicos.length;
 
 		return k;
 	}
