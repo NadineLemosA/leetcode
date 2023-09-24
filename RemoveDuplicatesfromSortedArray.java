2bd6a02edcc8df2fd9febb851545ec166aaa19da
Nadine de Lemos Aquino <nadinelemos2@gmail.com>
2023-09-23 21:55:18 -0300

    Exercicio funcionando, com falha no modo de teste da leetcode

--- RemoveDuplicatesfromSortedArray.java
+++ RemoveDuplicatesfromSortedArray.java
@@ -1,39 +1,31 @@
 package leetCode;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class RemoveDuplicatesfromSortedArray {
 	public static void main(String[] args) {
-		System.out.println(removeDuplicates(new int[] { 1, 1, 2, 3, 5, 5 })); // 1,1,1,2,2,2
+		System.out.println(removeDuplicates(new int[] { 1, 1, 1, 1, 1, 10 }));
 	}
 
 	public static int removeDuplicates(int[] nums) {
 		int k;
-		int[] listaDeUnicos = new int[nums.length]; //  a lista deve ser arraylist e achar modo de comparar int com interger
+		ArrayList<Integer> listaDeUnicos = new ArrayList<>();
 
-		listaDeUnicos[0] = nums[0];
+		listaDeUnicos.add(nums[0]);
 
 		for (int i = 0; i < nums.length - 1; i++) {
-			// System.out.println("i: " + listaDeUnicos.get(i));
-			if (listaDeUnicos[i] != nums[i + 1]) {
-				listaDeUnicos[i + 1] = nums[i + 1];
+			if (nums[i] != nums[i + 1]) {
+				listaDeUnicos.add(nums[i + 1]);
 			}
-
-			// for (int j = i + 1; j < nums.length; j++) {
-			// System.out.println("j: " + nums[j]);
-
-			// if (listaDeUnicos.get(i) != nums[j]) {
-			// listaDeUnicos.add(nums[j]);
-			// }
-			// }
-
-			System.out.println(Arrays.toString(listaDeUnicos));
-			System.out.println();
 		}
+
+		nums = listaDeUnicos.stream().mapToInt(Integer::intValue)
+				.toArray();
+		;
 
 		System.out.println(listaDeUnicos);
 
-		k = listaDeUnicos.length;
+		k = listaDeUnicos.size();
 
 		return k;
 	}
