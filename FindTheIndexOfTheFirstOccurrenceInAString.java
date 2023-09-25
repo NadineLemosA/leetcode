17734639016e358717c88079321634683008eb17
Nadine de Lemos Aquino <nadinelemos2@gmail.com>
2023-09-25 00:05:17 -0300

    wip - codigo funcionando com limite excedido no loop

--- FindTheIndexOfTheFirstOccurrenceInAString.java
+++ FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -1,8 +1,10 @@
 package leetCode;
 
+import java.util.ArrayList;
+
 public class FindTheIndexOfTheFirstOccurrenceInAString {
     public static void main(String[] args) {
-        System.out.println(strStr("sadbutsad", "sad"));
+        System.out.println(strStr("leetcode", "leeto"));
     }
 
     public static int strStr(String haystack, String needle) {
@@ -11,18 +13,29 @@ public class FindTheIndexOfTheFirstOccurrenceInAString {
 
         String combination = "";
         int indice = -1;
+        ArrayList<Integer> indexList = new ArrayList<>();
 
-        for (int i = 0; i < charactersNeedle.length; i++) {
-            for (int j = 0; j < charactersHaystack.length; j++) {
+        for (int i = 0; i < charactersHaystack.length; i++) { // excedendo o limite
+            for (int j = 0; j < charactersNeedle.length; j++) {
                 System.out.println("i: " + i);
                 System.out.println("j: " + j);
-                if (charactersNeedle[i] == charactersHaystack[j]) {
-                    combination = combination + String.valueOf(charactersHaystack[j]);
-                    System.out.println(combination);
+                if (charactersHaystack[i] == charactersNeedle[j]) {
+                    combination = combination + String.valueOf(charactersNeedle[j]);
+                    indexList.add(i);
+                    System.out.println("combinação: " + combination);
                     i++;
+                } else {
+                    combination = "";
                 }
             }
         }
+
+        if (needle.equals(combination)) {
+            indice = indexList.get(0);
+        }
+
+        System.out.println("indice:" + indice);
+        System.out.println(indexList);
 
         return indice;
     }
