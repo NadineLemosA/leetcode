1d5ba54ee6d802027e91a5962163490d85e9e309
Nadine de Lemos Aquino <nadinelemos2@gmail.com>
2023-10-01 19:10:40 -0300

    Corrige nomenclatura de variáveis

--- FindTheIndexOfTheFirstOccurrenceInAString.java
+++ FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -5,44 +5,43 @@ import java.util.ArrayList;
 public class FindTheIndexOfTheFirstOccurrenceInAString {
     public static void main(String[] args) {
         System.out.println(strStr("mississippi", "pi"));
-        // System.out.println(strStr("mississippi", "issip"));
     }
 
     public static int strStr(String haystack, String needle) {
         char[] charactersHaystack = haystack.toCharArray();
         char[] charactersNeedle = needle.toCharArray();
-        String combination = "";
+        String validCombination = "";
         ArrayList<Integer> indexList = new ArrayList<>();
-        int indice = -1;
-        int i = 0;
-        int j = 0;
+        int indexFirstValidCombination = -1;
+        int counterHaystack = 0;
+        int counterNeedle = 0;
 
-        for (i = 0; i < charactersHaystack.length; i++) {
+        for (counterHaystack = 0; counterHaystack < charactersHaystack.length; counterHaystack++) {
 
-            if (charactersNeedle[j] == charactersHaystack[i]) {
-                combination = combination + String.valueOf(charactersNeedle[j]);
-                indexList.add(i);
+            if (charactersNeedle[counterNeedle] == charactersHaystack[counterHaystack]) {
+                validCombination = validCombination + String.valueOf(charactersNeedle[counterNeedle]);
+                indexList.add(counterHaystack);
 
-                if (j < charactersNeedle.length - 1) {
-                    j++;
+                if (counterNeedle < charactersNeedle.length - 1) {
+                    counterNeedle++;
                 }
             } else {
 
                 if (!indexList.isEmpty()) {
-                    i = indexList.get(0);
+                    counterHaystack = indexList.get(0);
                 }
 
-                combination = "";
+                validCombination = "";
                 indexList.clear();
-                j = 0;
+                counterNeedle = 0;
             }
 
-            if (needle.equals(combination)) {
-                indice = indexList.get(0);
-                i = charactersHaystack.length;
+            if (needle.equals(validCombination)) {
+                indexFirstValidCombination = indexList.get(0);
+                counterHaystack = charactersHaystack.length;
             }
         }
 
-        return indice;
+        return indexFirstValidCombination;
     }
 }
