2ed005c05747de10b220a0826c610ee3db495cb6
Nadine de Lemos Aquino <nadinelemos2@gmail.com>
2023-10-17 18:20:22 -0300

    Adiciona exercicio resolvido utilizando a classe BigInteger

--- PlusOne.java
+++ PlusOne.java
@@ -1,35 +1,36 @@
 package leetCode;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 
 public class PlusOne {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(plusOne(new int[] {7,2,8,5})));
+        System.out.println(Arrays.toString(plusOne(
+                new int[] { 9, 2, 2, 3, 3, 7, 2, 0, 3, 6, 8, 5, 4, 7, 7, 5, 8, 0, 7, 0, 1, 2, 5, 4, 5, 2, 4 })));
     }
 
     public static int[] plusOne(int[] digits) {
-        String transformadoEmString = "";
+        String transformedToString = "";
 
         for (int i : digits) {
-            transformadoEmString += i;
+            transformedToString += i;
         }
 
-        long numeroGrande = Long.parseLong(transformadoEmString);
-        numeroGrande = numeroGrande + 1;
-        
-        char[] caracteres = Long.toString(numeroGrande).toCharArray(); 
+        BigInteger bigNumber = new BigInteger(transformedToString);
+        bigNumber = bigNumber.add(BigInteger.valueOf(1));
 
-        int[] retorno = new int[caracteres.length];
+        String backToString = new String(String.valueOf(bigNumber));
+
+        char[] caracteres = (backToString).toCharArray();
+
+        int[] result = new int[caracteres.length];
         int i = 0;
 
-        for (char unidade : caracteres) {
-            retorno[i] = Character.getNumericValue(unidade);
+        for (char unit : caracteres) {
+            result[i] = Character.getNumericValue(unit);
             i++;
         }
 
-        return retorno;
+        return result;
     }
 }
-// int[] array = {1, 2, 3, 4, 5};
-// array.toString(); pq isso nao funciona?
-// erro de tempo de execução para numeros gigantes
