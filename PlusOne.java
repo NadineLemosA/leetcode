8b03320b55035d1ef31786c1bccbefc16a96d5dd
Nadine de Lemos Aquino <nadinelemos2@gmail.com>
2023-10-19 16:46:27 -0300

    WIP - adiciona codigo modificado, funcionando para as opções dadas

--- PlusOne.java
+++ PlusOne.java
@@ -1,36 +1,47 @@
 package leetCode;
 
-import java.math.BigInteger;
 import java.util.Arrays;
 
 public class PlusOne {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(plusOne(
-                new int[] { 9, 2, 2, 3, 3, 7, 2, 0, 3, 6, 8, 5, 4, 7, 7, 5, 8, 0, 7, 0, 1, 2, 5, 4, 5, 2, 4 })));
+        System.out.println(Arrays.toString(plusOne(new int[] { 9, 9, 9, 9, })));
+        System.out.println(Arrays.toString(plusOne(new int[] { 9, 7, 9, 9 }))); // verificar ajuste para este caso
+        System.out.println(Arrays.toString(plusOne(new int[] { 7, 2, 3, 5 })));
     }
 
     public static int[] plusOne(int[] digits) {
-        String transformedToString = "";
+        boolean ultimoNumero = false;
 
-        for (int i : digits) {
-            transformedToString += i;
+        for (int i = digits.length - 1; i >= 0; i--) {
+
+            if (ultimoNumero == false && digits[i] == 9) {
+
+                if (digits[i] == 9 && digits[i - 1] != 9) {
+                    digits[i] = 0;
+                    digits[i - 1] = digits[i - 1] + 1;
+                }
+
+                if (digits[i] == 9 && digits[i - 1] == 9) {
+                    for (i = digits.length - 1; i >= 0; i--) {
+                        while (digits[i] == 9) {
+                            digits[i] = 0;
+                        }
+                    }
+                    int[] digitsResult = new int[digits.length + 1];
+                    digitsResult[0] = 1;
+                    System.arraycopy(digits, 0, digitsResult, 1, digits.length);
+                    digits = digitsResult;
+                }
+
+                ultimoNumero = true;
+            }
+
+            if (ultimoNumero == false && digits[i] != 9) {
+                digits[i] += 1;
+                ultimoNumero = true;
+            }
         }
 
-        BigInteger bigNumber = new BigInteger(transformedToString);
-        bigNumber = bigNumber.add(BigInteger.valueOf(1));
-
-        String backToString = new String(String.valueOf(bigNumber));
-
-        char[] caracteres = (backToString).toCharArray();
-
-        int[] result = new int[caracteres.length];
-        int i = 0;
-
-        for (char unit : caracteres) {
-            result[i] = Character.getNumericValue(unit);
-            i++;
-        }
-
-        return result;
+        return digits;
     }
 }
