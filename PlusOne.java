b5664de3c89e7e34dc3408ce778b3e864fae7d20
Nadine de Lemos Aquino <nadinelemos2@gmail.com>
2023-10-24 18:25:07 -0300

    Adiciona exercício concluido e aceito na plataforma

--- PlusOne.java
+++ PlusOne.java
@@ -4,41 +4,60 @@ import java.util.Arrays;
 
 public class PlusOne {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(plusOne(new int[] { 9, 9, 9, 9, })));
-        System.out.println(Arrays.toString(plusOne(new int[] { 9, 7, 9, 9 }))); // verificar ajuste para este caso
+        System.out.println(Arrays.toString(plusOne(new int[] { 9, 9, 9, 9 })));
         System.out.println(Arrays.toString(plusOne(new int[] { 7, 2, 3, 5 })));
+        System.out.println(Arrays.toString(plusOne(new int[] { 9, 7, 9, 9 })));
+        System.out.println(Arrays.toString(plusOne(new int[] { 9 })));
     }
 
     public static int[] plusOne(int[] digits) {
-        boolean ultimoNumero = false;
+        boolean lastNumber = false;
 
-        for (int i = digits.length - 1; i >= 0; i--) {
-
-            if (ultimoNumero == false && digits[i] == 9) {
-
-                if (digits[i] == 9 && digits[i - 1] != 9) {
-                    digits[i] = 0;
-                    digits[i - 1] = digits[i - 1] + 1;
-                }
-
-                if (digits[i] == 9 && digits[i - 1] == 9) {
-                    for (i = digits.length - 1; i >= 0; i--) {
-                        while (digits[i] == 9) {
-                            digits[i] = 0;
-                        }
-                    }
-                    int[] digitsResult = new int[digits.length + 1];
-                    digitsResult[0] = 1;
-                    System.arraycopy(digits, 0, digitsResult, 1, digits.length);
-                    digits = digitsResult;
-                }
-
-                ultimoNumero = true;
+        if (digits.length == 1) {
+            if (digits[0] == 9) {
+                int[] digitsResult = new int[digits.length + 1];
+                digitsResult[0] = 1;
+                digitsResult[1] = 0;
+                digits = digitsResult;
+            } else {
+                digits[0] = digits[0] + 1;
             }
 
-            if (ultimoNumero == false && digits[i] != 9) {
-                digits[i] += 1;
-                ultimoNumero = true;
+        } else {
+            for (int i = digits.length - 1; i >= 0; i--) {
+
+                if (lastNumber == false && digits[i] == 9) {
+
+                    if (digits[i] == 9 && digits[i - 1] != 9) {
+                        digits[i] = 0;
+                        digits[i - 1] = digits[i - 1] + 1;
+                    }
+
+                    if (digits[i] == 9 && digits[i - 1] == 9) {
+                        for (i = digits.length - 1; i >= 0; i--) {
+                            if (digits[i] == 9) {
+                                digits[i] = 0;
+                            } else {
+                                digits[i] = digits[i] + 1;
+                                i = 0;
+                            }
+                        }
+
+                        if (digits[0] == 0) {
+                            int[] digitsResult = new int[digits.length + 1];
+                            digitsResult[0] = 1;
+                            System.arraycopy(digits, 0, digitsResult, 1, digits.length);
+                            digits = digitsResult;
+                        }
+                    }
+
+                    lastNumber = true;
+                }
+
+                if (lastNumber == false && digits[i] != 9) {
+                    digits[i] += 1;
+                    lastNumber = true;
+                }
             }
         }
 
