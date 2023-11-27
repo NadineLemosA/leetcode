b5e58d02d26a7e69604fcb671baa9de66c8ce3f5
Nadine de Lemos Aquino <nadinelemos2@gmail.com>
2023-11-27 18:53:34 -0300

    fix: corrige exclusão do anderline ao 15 exercicio

--- ValidPalindrome.java
+++ ValidPalindrome.java
@@ -4,17 +4,15 @@ public class ValidPalindrome {
 
     public static void main(String[] args) {
         System.out.println(isPalindrome("ab_a"));
-        // System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
         // System.out.println(isPalindrome(" "));
     }
 
     public static boolean isPalindrome(String s) {
-        String sFormatada = s.replaceAll("[^a-zA-Z_0-9]", ""); // verificar como retirar o anderline
-        System.out.println(sFormatada);
-        String sInvertida = new StringBuilder(sFormatada).reverse().toString();
-        System.out.println(sInvertida);
+        String sFormatted = s.replaceAll("_", "").replaceAll("[^a-zA-Z_0-9]", "");
+        String sInverted = new StringBuilder(sFormatted).reverse().toString();
 
-        if (sFormatada.equalsIgnoreCase(sInvertida)) {
+        if (sFormatted.equalsIgnoreCase(sInverted)) {
             return true;
         } else {
             return false;
